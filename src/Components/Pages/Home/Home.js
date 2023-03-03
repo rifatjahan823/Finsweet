7bd5f8aab80baa84b9947a90ab8cc6c8d83ef8ab
rifatjahan823 <rifatjahan823@gmail.com>
2023-03-03 10:48:29 +0600

    complete podcast section

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './Hero/Hero';
+import Podcast from './Podcast/Podcast';
 
 const Home = () => {
     return (
         <div className='mx-auto w-11/12'>
             <Hero></Hero>
+            <Podcast></Podcast>
         </div>
     );
 };
